31ceb75b18e37b8eb3206dc6c3d367834b6d2215
Theodor908 <vasile.theodor908@gmail.com>
2026-02-26 02:19:59 +0200

    Skybox complete yey

--- include/scenes/p1_scene.hpp
+++ include/scenes/p1_scene.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "scenes/scene.hpp"
+#include "scenes/skybox.hpp"
 #include "shaders/shader.hpp"
+#include <glm/glm.hpp>
 
 class P1Scene : public Scene {
 public:
-    P1Scene() : Scene("Scene 1") {}
+    P1Scene() : Scene("Skybox") {}
 
     void Load() override;
     void Update() override;
@@ -12,8 +14,15 @@ public:
     void Unload() override;
 
 private:
+    Skybox skybox;
     Shader shader;
     unsigned int VAO = 0;
     unsigned int VBO = 0;
     unsigned int EBO = 0;
+    float rotationAngle = 0.0f;
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+     glm::vec3 cameraDir = glm::vec3(0.0f, 0.0f, -1.0f);
+    float yaw = -90.0f;
+    float pitch = 0.0f;
 };
